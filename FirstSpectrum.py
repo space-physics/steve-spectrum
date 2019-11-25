5fffc6fc1a339e44910948b55f618acbae080a28
Michael Hirsch, Ph.D <scivision@users.noreply.github.com>
2019-11-25 13:09:42 -0500

    init

---
+++ FirstSpectrum.py
@@ -0,0 +1,98 @@
+#!/usr/bin/env python
+"""
+python FirstSpectrumFigure1.py ~/data/2019GL083272
+
+Four main sources of discrepancy from paper figures:
+
+1. picking the same elevation bins
+2. flipping poleward / equatorward elevation direction
+3. spectral smoothing
+4. bin width
+"""
+import argparse
+from pathlib import Path
+import numpy as np
+import xarray
+import typing
+from datetime import datetime
+from matplotlib.pyplot import figure, show
+
+
+def load_spectrum(path: Path) -> np.ndarray:
+    """ load TRex spectrum text file from
+    https://doi.org/10.5281/zenodo.3552801
+
+    data is irregularly formatted, defying usual Numpy reads. Just do it the manual way.
+    """
+
+    path = Path(path).expanduser()
+    if not path.is_dir():
+        raise NotADirectoryError(
+            f"{path} not found; please download and extract the data from https://doi.org/10.5281/zenodo.3552801"
+        )
+
+    wavelengths = read_flattxt(path / "wavelength_per_bin.txt", 3)
+
+    dat_t0 = np.array(read_flattxt(path / "TREx_spectrograph_20180410_063045.txt", 4)).reshape((-1, len(wavelengths)))
+
+    dat_t1 = np.array(read_flattxt(path / "TREx_spectrograph_20180410_064015.txt", 4)).reshape((-1, len(wavelengths)))
+
+    time = [datetime(2018, 4, 10, 6, 30, 45), datetime(2018, 4, 10, 6, 40, 15)]
+
+    dat = xarray.DataArray(
+        np.stack((dat_t0, dat_t1)),
+        dims=("time", "elevation", "wavelength"),
+        coords={"time": time, "elevation": np.linspace(0, 180, 256), "wavelength": wavelengths},
+    )
+
+    return dat
+
+
+def read_flattxt(path: Path, skip_header: int) -> typing.List[float]:
+    """ read float data from irregularly shaped text file """
+    dat: typing.List[float] = []
+    with path.open("r") as f:
+        skip_rows(f, skip_header)
+        for line in f:
+            dat += list(map(float, line.split()))
+    return dat
+
+
+def skip_rows(f, rows: int):
+    """ skip rows of file f """
+    for _ in range(rows):
+        f.readline()
+
+
+def plot_speclines(dat: xarray.DataArray):
+
+    # by inspection of paper
+    elevation_deg = [
+        {"poleward": 108.0, "equatorward": 100.0, "steve": 104.0},
+        {"poleward": 123.0, "equatorward": 117.0, "steve": 120.0},
+    ]
+
+    for i in range(dat.shape[0]):
+        ax = figure().gca()
+        for k, v in elevation_deg[i].items():
+            j = abs(dat.elevation - v).argmin().item()
+            ax.plot(dat.wavelength, dat[i, j, :].values, label=k)
+        for w in (427.8, 557.7, 630.0):
+            ax.axvline(w, color="black", linestyle="--", alpha=0.5)
+        ax.legend()
+        ax.set_title(str(dat.time[i].values)[:-10])
+        ax.set_ylabel("Luminosity (Rayleighs)")
+        ax.set_xlabel("wavelength (nm)")
+        ax.grid(True)
+
+
+if __name__ == "__main__":
+    p = argparse.ArgumentParser()
+    p.add_argument("path", help="path to paper data directory")
+    P = p.parse_args()
+
+    dat = load_spectrum(P.path)
+
+    plot_speclines(dat)
+
+    show()
--- README.md
+++ README.md
@@ -1,2 +1,18 @@
+[![Actions Status](https://github.com/space-physics/steve-spectrum/workflows/ci_python/badge.svg)](https://github.com/space-physics/steve-spectrum/actions)
+
+
 # steve-spectrum
-plots and analysis for the STEVE aeronomy phenomenon
+
+Plots and analysis for the STEVE aeronomy phenomenon
+
+## Papers
+
+[First Observations From the TREx Spectrograph: The Optical Spectrum of STEVE and the Picket Fence Phenomena](https://doi.org/10.1029/2019GL083272)
+uses data from
+[Zenodo](https://doi.org/10.5281/zenodo.3552801)
+extracted by the user to ~/data/.
+Recreate Figures 1 & 2 from this paper by:
+
+```sh
+python FirstSpectrum.py ~/data/2019GL083272
+```
